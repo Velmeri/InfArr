008e320cd36748a223fa2dd5137d1346bb576072
Velmeri <danielmikhh2gmail.com>
2022-10-17 15:38:36 +0200

    I add SetSize(int s) & SetGrow(int s)

--- InfArr/Main.cpp
+++ InfArr/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
@@ -7,17 +8,23 @@ class infarr
 {
 	int capacity;
 	int size;
+	int grow;
 	T* arr;
 public:
 	infarr();
 
 	int GetSize();
 	int GetCapacity();
+
+	void SetSize(int s);
+	void SetGrow(int s); // a function should only perform one specific task. I see no reason to combine this with SetSize()
 };
 
 int main() {
 	infarr<int> arr1;
 	cout << arr1.GetCapacity();
+
+	arr1.SetSize(10);
 	cout << arr1.GetSize();
 
 	return 0;
@@ -28,6 +35,7 @@ infarr<T>::infarr()
 {
 	size = 0;
 	capacity = 0;
+	grow = 0;
 	arr = nullptr;
 }
 
@@ -41,4 +49,30 @@ template<class T>
 int infarr<T>::GetCapacity()
 {
 	return capacity;
+}
+
+template<class T>
+void infarr<T>::SetSize(int s)
+{
+	if (s >= 0)
+		size = s;
+	else {
+		//I didn't know much about the windows.h
+		HANDLE window = GetStdHandle(STD_OUTPUT_HANDLE); //But from what I understand this function extracts a link to the program (here on the console)
+		SetConsoleTextAttribute(window, 4); //And this function changes what will be displayed in window (more accurate, it change color of output)
+		cout << "\n\tError - wrong size\n";
+		SetConsoleTextAttribute(window, 15);
+		//Maybe I misunderstood something, but the main thing is that it works as I want
+	}
+}
+
+template<class T>
+void infarr<T>::SetGrow(int s)
+{
+	//I didn't know much about the windows.h
+	HANDLE window = GetStdHandle(STD_OUTPUT_HANDLE); //But from what I understand this function extracts a link to the program (here on the console)
+	SetConsoleTextAttribute(window, 4); //And this function changes what will be displayed in window (more accurate, it change color of output)
+	cout << "\n\tError - wrong size\n";
+	SetConsoleTextAttribute(window, 15);
+	//Maybe I misunderstood something, but the main thing is that it works as I want
 }
